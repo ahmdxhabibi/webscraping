4b72b67995fddf618a79b401a5ea4e95218f2ee5
ahmdxhabibi <ahmdxhabibi@gmail.com>
2023-10-10 10:14:59 +0700

    Scraping with selenium

--- tes_scraping_lagi.py
+++ tes_scraping_lagi.py
@@ -8,17 +8,16 @@ opsi = webdriver.ChromeOptions()
 opsi.add_argument('--headless')
 servis = Service('chromedriver.exe')
 driver = webdriver.Chrome(service=servis, options=opsi)
-# link = "https://shopee.co.id/search?keyword=headset"
 link = "https://shopee.co.id/Kaos-Polos-col.3971528"
 driver.set_window_size(1300,800)
 driver.get(link)
 
 ##selenium auto scroll
 scroll_ms = 500
-for i in range(1,7):
+for i in range(1,10):
     last = scroll_ms * i
-    to_do = "window.scrollTo(0, " + str(last)+")"
-    driver.execute_async_script(to_do)
+    to_do = "window.scrollTo(0,"+str(last)+")"
+    driver.execute_script(to_do)
     print("Loading " + str(i))
     time.sleep(1)
 
@@ -27,15 +26,18 @@ time.sleep(5)
 driver.save_screenshot("home.png")
 content = driver.page_source
 
-driver.quit()
+driver.quit() 
 
-data = BeautifulSoup(content, "html.parser")
+data = BeautifulSoup(content, "html.parser") ## Parsing data / Memisahkan data
 i = 1
-# print(data.encode("utf-8"))
 base_link = "https://shopee.co.id/" 
+## Inisialisasi sebagai list sebelum dimasukkan ke data excel
+list_nama, list_gambar, list_harga, list_link, list_terjual, list_lokasi = [], [], [], [], [], []
+
 for area in data.find_all("div", class_="col-xs-2-4 shopee-search-item-result__item"):
     print(i)
     i+=1
+    ## Sesuaikan dengan tag HTML pada web yang di scraping
     nama = area.find("div", class_="Af+5o- JKW-T3 U28b4N").get_text()
     gambar = area.find("img")["src"]
     harga = area.find("span", class_="wbhegq").get_text()
@@ -44,6 +46,7 @@ for area in data.find_all("div", class_="col-xs-2-4 shopee-search-item-result__i
     if terjual != None:
         terjual = terjual.get_text()
     lokasi = area.find("div", class_="rkY3va").get_text()
+    ## Pengecekkan apakah data sudah masuk
     print(nama)
     print(gambar)
     print(harga)
@@ -52,3 +55,16 @@ for area in data.find_all("div", class_="col-xs-2-4 shopee-search-item-result__i
     print(link)
     print("--------------------------------")
     print("")
+    ## Memasukkan data
+    list_nama.append(nama)
+    list_gambar.append(gambar)
+    list_harga.append(harga)
+    list_terjual.append(terjual)
+    list_lokasi.append(lokasi)
+    list_link.append(link)
+
+## Input data ke excel
+df = pandas.DataFrame({'Nama':list_nama, 'Gambar':list_gambar, 'Harga':list_harga, 'Terjual':list_terjual, 'Lokasi':list_lokasi, 'Link':list_link})
+writer = pandas.ExcelWriter('kaos.xlsx')
+df.to_excel(writer, 'Sheet1', index=False)
+writer.save()
